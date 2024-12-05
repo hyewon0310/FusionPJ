c246e10f06e845515eb82ec944bb3ce26232bb86
혜원 <20231089@kumoh.ac.kr>
2024-12-06 03:56:38 +0900

    written by Professor

--- src/InputOutputStream/ReadCharactersBlock.java
+++ src/InputOutputStream/ReadCharactersBlock.java
@@ -3,7 +3,7 @@ package InputOutputStream;
 import java.io.*;
 public class ReadCharactersBlock
 {
-    public static void main(String args[]) throws java.io.IOException {
+    public static void main(String[] args) throws java.io.IOException {
         byte[] buffer = new byte[80];
         int numberRead;
         while((numberRead = System.in.read(buffer)) >= 0) // 내용을 블록단위로 읽어 buffer에 저장
