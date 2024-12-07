38d354f2523d6d5653770f84658423163614a8de
hyewon0310 <20231089@kumoh.ac.kr>
2024-12-08 04:16:56 +0900

    돌아가는 코드 with 디버깅

--- src/Project/Main.java
+++
@@ -1,9 +0,0 @@
-package Project;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        server.run();
-        Client.test();
-    }
-}
