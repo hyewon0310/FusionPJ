26dc362ed2f768a1a876d99e3ac8efe37c044464
hyewon0310 <20231089@kumoh.ac.kr>
2024-12-07 18:15:35 +0900

    merge test용

--- src/Project/ClientHandler.java
+++ src/Project/ClientHandler.java
@@ -26,4 +26,55 @@ public class ClientHandler {
             System.err.println(ioException);
         }
     }
+
+    // 프로토콜 송신
+    private static void send(Protocol protocol) throws Exception {
+        try {
+            os.write(protocol.createPacket());
+            System.out.println("서버에게" + " 전송");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 }
+
+    // 프로토콜 수신
+//    private static Protocol receive(int type) throws Exception {
+//        // header부터 처리
+//
+//        byte[] header = new byte[Protocol.LEN_HEADER];
+//        Protocol protocol = new Protocol();
+//        // undefined 타입은 필요하면 추가해서 수정
+//        try {
+//            int totalReceived, readSize;
+//            do {
+//                totalReceived = 0;
+//                readSize = 0;
+//                is.read(header, 0, Protocol.LEN_HEADER);
+//                protocol.setPacketHeader(header);
+//
+//                byte[] buf = new byte[protocol.getBodyLength()];
+//                while (totalReceived < protocol.getBodyLength()) {
+//                    readSize = is.read(buf, totalReceived, protocol.getBodyLength() - totalReceived);
+//                    totalReceived += readSize;
+//                    if (readSize == -1) {
+//                        throw new Exception("통신오류: 연결 끊어짐");
+//                    }
+//                }
+//                protocol.setPacketBody(buf);
+//                if (protocol.getType() == Protocol.UNDEFINED)
+//                    throw new Exception("통신오류: 서버에서 오류 발생함");
+//                else if (protocol.getType() == type)
+//                    return protocol;
+//            } while (true); // 현재 필요한 응답이 아닐경우 무시하고 다음 응답을 대기
+//        } catch (IOException e) {
+//            throw new Exception("통신오류: 데이터 수신 실패함");
+//        }
+//    }
+//
+//
+//    // 선발 일정 및 비용 확인 조회 요청 및 확인
+//    public static String[] scheduleList_req() {
+//        Protocol protocol = new Protocol(Protocol.TYPE6_VIEW_REQ, Protocol.T6_CODE0_SELECTION_SCHEDULE_LIST);
+//    }
+//}
--- src/Project/Server.java
+++ src/Project/Server.java
@@ -9,49 +9,66 @@ import java.util.ArrayList;
 
 public class Server {
     // 서버 : 클라이언트 관계 = 1 : 1 관계
-    // 인텔리제이가 제안하는 것이
-    // 1. ServerSocket의 객체를 final로 만들수 있다.
-    // 2. ServerThread 배열과 clientCount 변수는 static 안사용해도 되지 않을까?
-    // 다음에 대해서는 언니랑 이야기 해보기
-
     private static ServerSocket serverSocket;  // 대기 소켓
     // 다중 처리 기술
     private static ServerThread clients[];
     private static int clientCount;
+    private InputStream is;
+    private OutputStream os;
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        server.run();
-    }
+//    public static void main(String[] args) throws Exception {
+//        Server server = new Server();
+//        server.run();
+//    }
 
     // 서버 생성자
     public Server() throws IOException {
-        serverSocket = new ServerSocket();
-        boolean on = true;
-        serverSocket.setReuseAddress(true);
-        // SO_TIMEOUT 옵션과 SO_KEEPALIVE 옵션 설정을 해줄지에 대해 얘기해보기
-
-        // 서버의 port 번호 설정
-        SocketAddress serverAddress = new InetSocketAddress(3000);
-        serverSocket.bind(serverAddress);
-
-        // pool로 구현할 수 있으면 pool로 구현 수정하기
+        serverSocket = new ServerSocket(7777);
         clients = new ServerThread[50]; // 최대 50명 수용 -> 수정 가능
         clientCount = 0;
     }
 
-    // 클라이언트에게 서버의 ip를 알려주는 메소드
-    public static String getServerAddress() throws IOException {
-            InetAddress localHost = InetAddress.getLocalHost();
-            return localHost.getHostAddress();
-        }
-    }
+    // 구동
+//    public void run() throws IOException {
+//        while (serverSocket != null) {
+//            // 클라이언트 접속 대기 및 통신 담당 소켓 담당
+//            Socket commSocekt = serverSocket.accept();  // 통신 소켓
+//            //DB 연결 추가하기
+//
+//            addThread(commSocekt, conn);
+//        }
+//    }
 
+    //  쓰레드 동기화
+//    public synchronized void addThread(Socket socket, Connection conn) throws Exception {
+//        if (clientCount < clients.length) {
+//            clients[clientCount] = new ServerThread(socket, conn);
+//            clients[clientCount].start();
+//            clientCount++;
+//        } else {
+//            System.out.println("Client refused: maximum " + clients.length + " reached.");
+//        }
+//    }
 
-        // 구동
-    public void run() {
-        // 클라이언트 접속 대기 및 통신 담당 소켓 담당
-        Socket commSocekt = serverSocket.accept();  // 통신 소켓
-        //DB 연결 추가하기
-    }
+//    public synchronized static void remove(int targetPortID) throws IOException {
+//        int pos = findClient(targetPortID);
+//        if (pos >= 0) {
+//            ServerThread toTerminate = clients[pos];    // 변수 네이밍은 나중에 생각
+//            if (pos < clientCount - 1)
+//                for (int i = pos + 1; i < clientCount; i++)
+//                    clients[i - 1] = clients[i];
+//            clientCount--;
+//            // System.out.print("Current Using Client Number : ");
+//            // System.out.print(clientCount);
+//            toTerminate.close();
+//            toTerminate.stop();
+//        }
+//    }
+//
+//    public static int findClient(int targetPortID) {
+//        for (int i = 0; i < clientCount; i++)
+//            if (clients[i].getPortID() == targetPortID)
+//                return i;
+//        return -1;
+//    }
 }
