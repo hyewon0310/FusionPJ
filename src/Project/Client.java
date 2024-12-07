ef694c284f42947af953f47dca80fb1ecc0ec031
hyewon0310 <20231089@kumoh.ac.kr>
2024-12-07 21:51:31 +0900

    test하기 위한 commit

--- src/Project/Client.java
+++ src/Project/Client.java
@@ -4,8 +4,13 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-    public static void main(String[] args) throws Exception{
+    public static void test() throws Exception{
         ClientHandler clientHandler = new ClientHandler();
+//        String[] result = clientHandler.scheduleList_viewReq();
+//        String message = "선발 일정 정보 : ";
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(message + result[i]);
+//        }
 //        Protocol protocol = new Protocol();
 //        byte[] buffer = protocol.getPacket();
 //        System.out.print(clientHandler.login("20231089", "123456")[0]);
